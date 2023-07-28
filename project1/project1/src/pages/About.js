f82f7ec6be6eb469fe58242c3abb6be0c6b65385
Diana Gregorio <diana.gregorio588@gmail.com>
2023-07-28 15:07:17 +0800

    append About Us page contents

--- project1/project1/src/pages/About.js
+++ project1/project1/src/pages/About.js
@@ -1,11 +1,22 @@
-
+import caption from '../img/relax.jpg';
 const About = () => {
 
     return(
 
-        <div className="About">
-            
-            <h2> How We Start </h2>
+        <div className="Col" >
+            <div className="text-center Col-6">
+                <h2> About Us </h2>
+                    <p>In a world inundated with emails, fake news, status updates,
+                        smartphones and connected watches, we exist to bring you back 
+                        to the moment.
+                    </p><br />
+                    <p>This is the moment our customer finds balance; when they stop 
+                        focusing on what they should be doing and intead focus on what 
+                        they want to do.</p>
+            </div>
+            <div className="Col-6">
+                <img src={caption} alt="girl on kayak" />
+            </div>
 
 
         </div>
--- project1/project1/src/pages/Home.js
+++ project1/project1/src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="Container Home bgmain text-center">
             <h2 className='h1 textwhite' >Welcome to the Vacation Island</h2>
             <h4 className="h4 textwhite">A Paradise within Paradise</h4>
-            <Button className='btn-success btn-lg '>Plan Your Adventure Now</Button>
+            <Button className='btn-success btn-lg'>Plan Your Adventure Now</Button>
         </div>
 
     );
